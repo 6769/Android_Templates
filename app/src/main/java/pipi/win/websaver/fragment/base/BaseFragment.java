e5fb65dff8291677d788ed3d94c1d2edbd5a1f23
5pipi <5pipitk@gmail.com>
2018-09-27 01:44:16 +0800

    add fragment base folder

---
+++ app/src/main/java/pipi/win/websaver/fragment/base/BaseFragment.java
@@ -0,0 +1,41 @@
+package pipi.win.websaver.fragment.base;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.widget.Toast;
+
+
+/**
+ * Created by pipi6 on 2017/8/4.
+ */
+
+public class BaseFragment extends Fragment {
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+
+    protected void mkToast(String msg){
+        Toast.makeText(getContext(),msg,Toast.LENGTH_SHORT).show();
+    }
+    protected void mkToastL(String msg){
+        Toast.makeText(getContext(),msg,Toast.LENGTH_LONG).show();
+    }
+
+
+}
